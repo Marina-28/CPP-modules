b3a4115bde58be29999e1c46964b8e1a0537cac3
Marina-28 <greatundeadmoose@gmail.com>
2022-04-02 09:40:40 +0200

    hpp file

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -7,21 +7,20 @@
 class ClapTrap
 {
 private:
-    /* data */
+    std::string _name;
+    int         _hit_points;
+    int         _energy_points;
+    int         _attack_damage;
+
 public:
     ClapTrap();
     ClapTrap(const ClapTrap & other);
 	ClapTrap & operator=(const ClapTrap & other);
     ~ClapTrap();
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
 };
-
-ClapTrap::ClapTrap(/* args */)
-{
-}
-
-ClapTrap::~ClapTrap()
-{
-}
-
 
 #endif
