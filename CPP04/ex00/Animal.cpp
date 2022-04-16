131b9cb7a44c3dfb1161865afd1a495e6debde55
Marina-28 <ermoshkina.marina1@gmail.com>
2022-04-16 18:34:14 +0200

    cpp04/ex00

---
+++ CPP04/ex00/Animal.cpp
@@ -0,0 +1,38 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    std::cout << "Animal default constructor called\n";
+    type = "Animal";
+}
+
+Animal::Animal(const Animal & other)
+{
+    std::cout << "Animal copy constructor called\n";
+    this->type = other.type;
+}
+
+Animal & Animal::operator=(const Animal & other)
+{
+    std::cout << "Animal copy assignment operator called\n";
+    this->type = other.type;
+    return *this;
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal destructor called\n";
+}
+
+/*******************************************************************/
+
+std::string Animal::getType() const
+{
+    return this->type;
+}
+
+void Animal::makeSound() const
+{
+    std::cout << "I am Animal, I don't make sound :/\n";
+}
+
