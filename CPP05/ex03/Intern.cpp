2bae052bac6c18b16e45b8dcaa8cf016d831dedc
Marina-28 <ermoshkina.marina1@gmail.com>
2022-04-21 18:05:40 +0200

    makeForm

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -20,3 +20,34 @@ Intern::~Intern()
     std::cout << "Intern destructor is called\n";
 }
 
+Form * Intern::makeForm(std::string name, std::string target)
+{
+    std::string forms[3] = {"ShrubberyCreationForm", \
+    "PresidentialPardonForm", "RobotomyRequestForm"};
+    int i = 0;
+    Form *form;
+
+    while (i < 3)
+    {
+        if (name.compare(forms[i]) == 0)
+            break;
+        i++;
+    }
+    switch (i)
+    {
+    case 0:
+        form = new ShrubberyCreationForm(target);
+        break;
+    case 1:
+        form = new PresidentialPardonForm(target);
+        break;
+    case 2:
+        form = new RobotomyRequestForm(target);
+        break;
+    default:
+        std::cout << "Form " << name << " doesn't exist.\n";
+        break;
+    }
+    std::cout << "Intern creates form " << name << std::endl;
+    return form;
+}
