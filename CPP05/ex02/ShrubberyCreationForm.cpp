1dbe332c932a4b6d29ba70fce5052af25a38b9ef
Marina-28 <greatundeadmoose@gmail.com>
2022-04-21 12:16:07 +0200

    ShrubberyCreationForm constructors

--- CPP05/ex02/ShrubberyCreationForm.cpp
+++ CPP05/ex02/ShrubberyCreationForm.cpp
@@ -1,9 +1,35 @@
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(/* args */)
+ShrubberyCreationForm::ShrubberyCreationForm() : Form("ShrubberyCreationForm", 145, 137)
 {
+    std::cout << "ShrubberyCreationForm default constructor is called\n";
+    _target = "Target";
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) \
+: Form("ShrubberyCreationForm", 145, 137)
+{
+    std::cout << "ShrubberyCreationForm arguments constructor is called\n";
+    _target = target;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm & other) \
+: Form("ShrubberyCreationForm", 145, 137)
+{
+    std::cout << "ShrubberyCreationForm copy constructor is called\n";
+    _target = other._target;
+}
+
+ShrubberyCreationForm & ShrubberyCreationForm::operator=(const ShrubberyCreationForm & other)
+{
+    std::cout << "ShrubberyCreationForm copy assignment operator is called\n";
+    if (this == &other)
+        return *this;
+    _target = other._target;
+    return (*this);
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
+    std::cout << "ShrubberyCreationForm destructor is called\n";
 }
