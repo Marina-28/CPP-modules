f387be2d4617f8621b1f866146805f74f1d9d063
Marina-28 <ermoshkina.marina1@gmail.com>
2022-04-22 08:54:05 +0200

    ex00

---
+++ CPP06/ex00/Convertation.hpp
@@ -0,0 +1,36 @@
+#ifndef CONVERTATION_HPP
+#define CONVERTATION_HPP
+
+#include <iostream>
+#include <string>
+
+class Convertation
+{
+private:
+    std::string _data;
+    char _char;
+    int _int;
+    double _double;
+    float _float;
+public:
+    Convertation();
+    Convertation(std::string data);
+    Convertation(const Convertation & other);
+    Convertation & operator=(const Convertation & other);
+    ~Convertation();
+
+    
+};
+
+Convertation::Convertation(/* args */)
+{
+}
+
+Convertation::~Convertation()
+{
+}
+
+
+
+
+#endif
---
+++ CPP06/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Enter one arguments\n";
+        return 1;
+    }
+
+}
